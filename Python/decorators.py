8adcfd9786a4b0a37924f4b3d28321247930789c
lichun <i@lichun.me>
2016-09-13 10:59:47 +0800

    Add python retry decorators

---
+++ Python/decorators.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+def retry(attempt=3):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kw):
+            att = 0
+            last_except = None
+            while att < attempt:
+                try:
+                    return func(*args, **kw)
+                except Exception as e:
+                    att += 1
+                    last_except = e
+            else:
+                raise last_except
+        return wrapper
+    return decorator
