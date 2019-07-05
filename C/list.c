355990dcc3c94ef9c66bd627333e8516e3761bad
lichun <i@lichun.me>
2019-07-05 11:27:10 +0800

    Add del insert function.

--- C/list.c
+++ C/list.c
@@ -23,7 +23,7 @@ struct Node *end(struct Node *node)
     return end(node->next);
 }
 
-void push(struct Node *head, int value)
+void append(struct Node *head, int value)
 {
     struct Node *new_node = create(value);
     end(head)->next = new_node;
@@ -51,17 +51,64 @@ int length(struct Node *head)
     return count;
 }
 
+struct Node *get(struct Node *head, int index)
+{
+    struct Node *node = head;
+    for (int i = 0; i < index - 1; i++)
+    {
+        node = node->next;
+    }
+    return node;
+}
+
+void insert(struct Node *head, int index, int value)
+{
+    struct Node *node = get(head, index);
+    struct Node *new_node = create(value);
+
+    new_node->next = node->next;
+    node->next = new_node;
+}
+
+void del(struct Node **head, int value)
+{
+    struct Node *first = *head;
+
+    if (first->value == value)
+    {
+        *head = first->next;
+        free(first);
+        return;
+    }
+
+    for (struct Node *node = first; node != NULL; node = node->next)
+    {
+        if (node->next->value == value)
+        {
+            free(node->next);
+            node->next = node->next->next;
+            break;
+        }
+    }
+}
+
 int main()
 {
     struct Node *head = create(0);
 
     for (int i = 1; i < 9; i++)
     {
-        push(head, i);
+        append(head, i);
     }
 
+    insert(head, 3, 12);
+    insert(head, 4, 13);
     print(head);
-    printf("length: %d \n", length(head));
+
+    del(&head, 0);
+    del(&head, 12);
+    del(&head, 8);
+    print(head);
 
     return 0;
 }
