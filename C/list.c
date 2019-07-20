aaee5d0707504ffdf40bdb30b2351f5a149dfe73
lichun <i@lichun.me>
2019-07-20 15:18:45 +0800

    Change use list struct.

--- C/list.c
+++ C/list.c
@@ -7,35 +7,45 @@ struct Node
     struct Node *next;
 };
 
+struct List
+{
+    size_t size;
+    struct Node *head;
+    struct Node *end;
+};
+
+struct List *new_list()
+{
+    struct List *list = (struct List *)malloc(sizeof(struct List));
+    list->size = 0;
+    list->head = NULL;
+    list->end = NULL;
+
+    return list;
+}
+
 struct Node *create(int value)
 {
     struct Node *node = (struct Node *)malloc(sizeof(struct Node));
     node->value = value;
     node->next = NULL;
+
+    return node;
 }
 
-struct Node *end(struct Node *node)
+void append(struct List *list, int value)
 {
-    if (node->next == NULL)
-        return node;
-
-    return end(node->next);
-}
-
-void append(struct Node *head, int value)
-{
-    struct Node *new_node = create(value);
-    end(head)->next = new_node;
-}
-
-int length(struct Node *head)
-{
-    int count = 0;
-    for (struct Node *node = head; node != NULL; node = node->next)
+    struct Node *node = create(value);
+    if (list->head == NULL)
     {
-        count++;
+        list->head = node;
     }
-    return count;
+    else
+    {
+        list->end->next = node;
+    }
+    list->end = node;
+    list->size++;
 }
 
 struct Node *get(struct Node *head, int index)
@@ -48,30 +58,36 @@ struct Node *get(struct Node *head, int index)
     return node;
 }
 
-void insert(struct Node **head, int index, int value)
+void insert(struct List *list, int index, int value)
 {
+    struct Node **head = &(list->head);
     struct Node *new_node = create(value);
 
     if (index == 0)
     {
         new_node->next = *head;
         *head = new_node;
-        return;
+    }
+    else
+    {
+        struct Node *node = get(*head, index);
+        new_node->next = node->next;
+        node->next = new_node;
     }
 
-    struct Node *node = get(*head, index);
-    new_node->next = node->next;
-    node->next = new_node;
+    list->size++;
 }
 
-void del(struct Node **head, int value)
+void del(struct List *list, int value)
 {
+    struct Node **head = &(list->head);
     struct Node *first = *head;
 
     if (first->value == value)
     {
         *head = first->next;
         free(first);
+        list->size--;
         return;
     }
 
@@ -81,6 +97,7 @@ void del(struct Node **head, int value)
         {
             free(node->next);
             node->next = node->next->next;
+            list->size--;
             break;
         }
     }
@@ -96,12 +113,11 @@ void swap(struct Node *one, struct Node *two)
     one->next = two;
 }
 
-void sort(struct Node *head)
+void sort(struct List *list)
 {
-    int len = length(head);
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < list->size; i++)
     {
-        for (struct Node *node = head; node->next != NULL; node = node->next)
+        for (struct Node *node = list->head; node->next != NULL; node = node->next)
         {
             if (node->value < node->next->value)
                 swap(node, node->next);
@@ -109,40 +125,39 @@ void sort(struct Node *head)
     }
 }
 
-void print(struct Node *head)
+void print(struct List *list)
 {
-    struct Node *node = head;
-
+    struct Node *node = list->head;
     while (node)
     {
         printf("%d ", node->value);
         node = node->next;
     }
-    puts("\n");
+    printf("\tlength: %ld\n", list->size);
 }
 
 int main()
 {
-    struct Node *head = create(0);
+    struct List *list = new_list();
 
-    for (int i = 1; i < 9; i++)
+    for (int i = 0; i < 9; i++)
     {
-        append(head, i);
+        append(list, i);
     }
-    print(head);
+    print(list);
 
-    insert(&head, 0, 9);
-    insert(&head, 9, 9);
-    print(head);
+    insert(list, 0, 9);
+    insert(list, 9, 9);
+    print(list);
 
-    sort(head);
-    print(head);
+    sort(list);
+    print(list);
 
-    del(&head, 0);
-    del(&head, 5);
-    del(&head, 9);
-    del(&head, 10); // non existent value
-    print(head);
+    del(list, 0);
+    del(list, 5);
+    del(list, 9);
+    del(list, 10); // non existent value
+    print(list);
 
     return 0;
 }
